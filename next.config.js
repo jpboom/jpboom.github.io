389058faf711cad360c53dee05e731e32fcd708f
jpboom <jesse.peereboom@gmail.com>
2022-03-05 19:53:38 +1100

    Get build working...

--- next.config.js
+++ next.config.js
@@ -1,27 +1,22 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
-const withNextOptimizedImages = require('next-optimized-images');
-
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-module.exports = withNextOptimizedImages(
-  withMDX({
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.node = {
-          fs: 'empty',
-        };
-      }
-      return config;
-    },
-    pageExtensions: ['js', 'jsx', 'mdx'],
-    target: 'serverless',
-    basePath: ghPages ? '/blog/' : '',
-    assetPrefix: ghPages ? '/blog/' : '',
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  })
-);
+module.exports = withMDX({
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: 'empty',
+  //     };
+  //   }
+  //   return config;
+  // },
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  basePath: ghPages ? '/blog/' : '',
+  assetPrefix: ghPages ? '/blog/' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
